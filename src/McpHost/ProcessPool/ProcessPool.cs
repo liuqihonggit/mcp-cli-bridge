a5f062eac66cd8c931df85af5afa50f71e61a01e
liuqihong <540762622@qq.com>
2026-04-29 05:34:59 +0800

    fix: ProcessPool Dispose 方法优化
    - 使用 ConfigureAwait(false).GetAwaiter().GetResult() 替代 .Wait()
    - 避免潜在的死锁问题
    - 符合异步最佳实践

--- src/McpHost/ProcessPool/ProcessPool.cs
+++ src/McpHost/ProcessPool/ProcessPool.cs
@@ -394,9 +394,7 @@ public sealed class ProcessPool : IProcessPool
 
     public void Dispose()
     {
-        // 同步 Dispose 使用 Wait 等待异步操作完成
-        // 这是 Dispose 模式的标准做法
-        DisposeAsync().AsTask().Wait();
+        DisposeAsync().AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
     }
 
     #endregion
