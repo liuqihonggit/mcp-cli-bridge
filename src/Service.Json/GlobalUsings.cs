930b15de00459d549eabf6eacb27392d63f38990
liuqihong <540762622@qq.com>
2026-04-29 05:11:59 +0800

    chore: remove old Service.Json and Service.Json.Contracts project directories
    - Delete src/Service.Json directory
    - Delete src/Service.Json.Contracts directory
    - All projects compile successfully

--- src/Service.Json/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
-global using Common.Contracts.Models;
-global using System.Text.Json.Serialization.Metadata;
-global using McpProtocol.Contracts;
-global using Service.Json.Schema;
-global using Common.Contracts;
-global using Common.Contracts.Security;
